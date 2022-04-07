8d2e682f3803344289b2373b503c948eb3372535
Lehi <lehi_alcantara@byu.edu>
2022-04-06 19:06:33 -0600

    initial

---
+++ src/IT252HW2.64/src/IT252HW2.64.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : IT252HW2.64.c
+ Author      : [your name]
+ Version     :
+ Description : HW problem 2.64
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int any_odd_one(unsigned x)
+{
+	// your code goes here
+	return 0;
+}
+
+int main(void) {
+	printf("IT252HW2.64\n"); /* prints IT252HW2.64 */
+	printf("any_odd_one(2) returns %d, should be 1\n", any_odd_one(2));
+	printf("any_odd_one(16) returns %d, should be 0\n", any_odd_one(16));
+	printf("any_odd_one(12345) returns %d, should be 1\n", any_odd_one(12345));
+	return EXIT_SUCCESS;
+}
+
---
+++ src/IT252HW2.65/src/IT252HW2.65.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : IT252HW2.65.c
+ Author      : [your name]
+ Version     :
+ Description : HW problem 2.65
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int odd_ones(unsigned x)
+{
+	// your code goes here
+	return 0;
+}
+
+int main(void) {
+	printf("IT252HW2.65\n"); /* prints IT252HW2.65 */
+	printf("odd_ones(2) returns %d, should be 1\n", odd_ones(2));
+	printf("odd_ones(200) returns %d, should be 1\n", odd_ones(200));
+	printf("odd_ones(12345) returns %d, should be 0\n", odd_ones(12345));
+	return EXIT_SUCCESS;
+}
+
